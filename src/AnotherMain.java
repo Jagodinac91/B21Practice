b442211aa2cac76a6f8c281b91493eca14d31ba8
Jagodinac91 <74147076+Jagodinac91@users.noreply.github.com>
2020-11-11 08:47:25 -0800

    Printing "Hello" one more time - branching

--- src/AnotherMain.java
+++ src/AnotherMain.java
@@ -2,5 +2,6 @@ public class AnotherMain {
     public static void main(String[] args) {
         //first line of code
         //second line of code
+        System.out.println("Hello for the third time!!!");
     }
 }
