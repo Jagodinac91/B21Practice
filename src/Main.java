1436fd6b9a227cb0e973866fc4d0b59a0aa7dac3
Jagodinac91 <74147076+Jagodinac91@users.noreply.github.com>
2020-11-11 08:32:42 -0800

    Printing "world" and adding a comment

--- src/Main.java
+++ src/Main.java
@@ -2,6 +2,7 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Hello");
         //First line of comment
-
+        System.out.println("World");
+        //Second line of comment
     }
 }
